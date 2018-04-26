1f31e40c4bbfa7f975c7cada1c4d4eeb8f25e8c8
Roland Tecson <roland_tecson@hotmail.com>
2018-04-26 10:52:24 -0700

    Update with code from lecture

--- w3d4-networking-demo/ViewController.m
+++ w3d4-networking-demo/ViewController.m
@@ -42,19 +42,24 @@
     
     // Create a NSURLSessionDataTask object that will execute the URL and get a response
     NSURLSessionDataTask *dataTask = [[NSURLSession sharedSession] dataTaskWithURL:url completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
-        // Handle response here
+        // Handle response here, we're still in the background thread
         if (error != nil) {
+            // If error is not nil, an error was encountered
             dispatch_async(dispatch_get_main_queue(), ^{
+                // Back at main queue
                 self.label.text = @"Error";
                 NSLog(@"Error: %@", error);
             });
             return;
         }
 
+        // Get here if there's no error
         dispatch_async(dispatch_get_main_queue(), ^{
+            // Back at main queue
             self.label.text = @"Parsing";
         });
         
+        // Parse the server's response
         [self processResponse:data];
     }];
     
@@ -63,18 +68,22 @@
 }
 
 - (void)processResponse:(NSData *)responseData {
+    // Convert data to JSON (parsing)
     NSError *error = nil;
     id json = [NSJSONSerialization JSONObjectWithData:responseData
                                     options:0
                                       error:&error];
     if (error != nil) {
+        // Error in parsing JSON
         dispatch_async(dispatch_get_main_queue(), ^{
+            // Back at main queue
             self.label.text = @"Error";
             NSLog(@"Error: %@", error);
         });
         return;
     }
     
+    // Cast JSON to NSDictionary only if it is truly a dictionary
     if ([json isKindOfClass:[NSDictionary class]]) {
         NSDictionary *jsonDict = (NSDictionary *)json;
         dispatch_async(dispatch_get_main_queue(), ^{
@@ -86,6 +95,7 @@
 
 - (void)processResponseDictionary:(NSDictionary *)responseDictionary {
     NSLog(@"jsonDict: %@", responseDictionary);
+    // Walk through dictionary, etc...
 }
 
 - (void)makeAfNetworkRequest {
@@ -103,22 +113,21 @@
     AFURLSessionManager *afUrlSessionManager = [[AFURLSessionManager alloc] initWithSessionConfiguration:configuration];
     
     // Create a NSURLSessionDataTask object that will execute the URL and get a response
-    NSURLSessionDataTask *dataTask = [afUrlSessionManager dataTaskWithRequest:urlRequest uploadProgress:nil downloadProgress:nil completionHandler:^(NSURLResponse * _Nonnull response, id  _Nullable responseObject, NSError * _Nullable error) {
+    NSURLSessionDataTask *dataTask = [afUrlSessionManager dataTaskWithRequest:urlRequest uploadProgress:nil downloadProgress:nil completionHandler:^(NSURLResponse * _Nonnull response, id _Nullable responseObject, NSError * _Nullable error) {
+        // In AFNetworking, the completion block is already in the main thread
         // Handle response here
         if (error != nil) {
             self.label.text = @"Error";
             NSLog(@"Error: %@", error);
         }
         
-        self.label.text = @"Parsing";
-
+        // In AFNetworking, the response is already parsed into a JSON object
         if ([responseObject isKindOfClass:[NSDictionary class]]) {
             [self processResponseDictionary:responseObject];
         }
         self.label.text = @"Completed!";
     }];
     
-    // Create a NSURLSessionDataTask object that will execute the URL and get a response
     // Execute the task in a background thread
     [dataTask resume];
 }
